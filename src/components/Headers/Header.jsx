a55490e4ca47cec3085d27ed946934087bce1c42
dbek59141@gmail.com <dbek59141@gmail.com>
2024-05-15 12:10:16 +0500

    my title

--- src/components/Headers/Header.jsx
+++ src/components/Headers/Header.jsx
@@ -2,7 +2,7 @@ import styles from "../Headers/header.module.css"
 const Header = () => {
   return (
     <div>
-
+      <h1>Doniyor bugun projectni boshlab oldi</h1>
       <img src="../../../public/logo.svg" alt="" />
     </div>
   )
