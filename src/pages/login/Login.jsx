587bfe3e89799e1a2a0d0ebee1dcc7cfb2eb896d
Doniyor <dbek59141@gmail.com>
2024-06-18 02:29:18 -0700

    done auth login validation

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,71 +1,43 @@
 import styles from "./login.module.css";
 import Image from "../../assets/FrameImg.png";
 import { ToastContainer, toast } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
-import { useState } from "react";
+import "react-toastify/dist/ReactToastify.css";
+import { Formik } from "formik";
 
-// const initialValues = {
-//   email: "",
-//   password: "",
-// };
+const initialValues = {
+  email: "",
+  password: "",
+};
 
 const Login = () => {
-
-  
-  // handleValidate
-  // const handleValidate = (values) => {
-  //   const errors = {};
-  //   if (!values.email) {
-  //     errors.email = "Required";
-  //   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-  //     errors.email = "Invalid email address";
-  //   }
-  //   if (!values.password) {
-  //     errors.password = "Required";
-  //   } else if ( values.password.length < 8 ){
-  //     errors.password = "Min 8"
-  //   }
-
-  //     return errors;
-  // };
-
-  // // Handle submit
-  // const onSubmit = (values, { setSubmitting }) => {
-  //   setTimeout(() => {
-  //     alert(JSON.stringify(values, null, 2));
-  //     setSubmitting(false);
-  //   }, 400);
-  // };
-
-  const [form,setForm] = useState({
-    email: "",
-    password: ""
-  })
-
-  const handleChange = (e)=> {
-    setForm({
-      ...form,
-      [e.target.value] : e.target.value
-    })
-    console.log(form);
-  }
-
-
-
-  const handleSubmit = (e)=> {
-    e.preventDefault();
-    const regex = /^[^\s@]+@[^\s@]+\- [^\s@]{2,}$/i;
-    if(!regex.test(form?.email)){
-      toast.error ('This is not a valid email')
+  const handleValidate = (values) => {
+    const errors = {};
+    if (!values.email) {
+      errors.email = "Required";
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+      errors.email = "Invalid email address";
+    }
+    if (!values.password) {
+      errors.password = "Required";
+    } else if (values.password.length < 8) {
+      errors.password = "Min 8";
     }
 
-    if(!form?.password) {
-      toast.error('password is required')
+    return errors;
+  };
+
+  // Handle submit
+  const onSubmit = (values, { setSubmitting }) => {
+    const loginAdmin = "admin@gmail.com";
+    const passAdmin = "87654321";
+    if (values.email === loginAdmin && values.password === passAdmin) {
+      toast.success("Success");
+    } else{
+      toast.error("Login or password is incorrect")
     }
-    else if(form?.password.length < 4) {
-      toast.error("password must be more than 4 characters")
-    }
-  }
+
+    setSubmitting(false)
+  };
 
   return (
     <div className={styles.content}>
@@ -78,50 +50,72 @@ const Login = () => {
       {/* Wrapper item */}
 
       <div className={styles.contentWrapper}>
-        <>
-          {/* Form */}
+        {/* Form */}
+        
+          <Formik
+            initialValues={initialValues}
+            validate={handleValidate}
+            onSubmit={onSubmit}
+          >
+            {({
+              values,
+              errors,
+              touched,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              isSubmitting,
+              /* and other goodies */
+            }) => (
+              <>
+              <ToastContainer/>
+                <form className={styles.form} onSubmit={handleSubmit}>
+                  {/* Title */}
+                  <h1 className={styles.title}>Login</h1>
 
-          <ToastContainer>
-            <form className={styles.form} onSubmit={(e)=>handleSubmit(e.target.value)}>
-              {/* Title */}
-              <h1 className={styles.title}>Login</h1>
+                  {/* Login */}
+                  <input
+                    className={styles.input}
+                    name="email"
+                    type="text"
+                    placeholder="Email"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.email}
+                  />
+                  <small className={styles.errors}>
+                    {errors.email && touched.email && errors.email}
+                  </small>
 
-              {/* Login */}
-              <input
-                className={styles.input}
-                name="email"
-                type="text"
-                placeholder="Email"
-                onChange={handleChange}
-              />
-              {/* <small className={styles.errors}>
-                  {errors.email && touched.email && errors.email}
-                </small> */}
+                  {/* Password */}
+                  <input
+                    className={styles.input}
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.password}
+                  />
+                  <small className={styles.errors}>
+                    {errors.password && touched.password && errors.password}
+                  </small>
 
-              {/* Password */}
-              <input
-                className={styles.input}
-                name="password"
-                type="password"
-                placeholder="Password"
-                onChange={handleChange}
-              />
-              {/* <small className={styles.errors}>
-                  {errors.password && touched.password && errors.password}
-                </small> */}
+                  {/* Submit btn */}
+                  <button
+                    className={styles.btn}
+                    type="submit"
+                    disabled={isSubmitting}
+                  >
+                    Login
+                  </button>
 
-              {/* Submit btn */}
-              <button
-                className={styles.btn}
-                type="submit"
-                // disabled={isSubmitting}
-              >
-                Login
-              </button>
-
-              {/* <pre>{JSON.stringify(values, null, 20)}</pre> */}
-            </form>
-      </div>
+                  <pre>{JSON.stringify(values, null, 20)}</pre>
+                </form>
+              </>
+            )}
+          </Formik>
+          </div>
     </div>
   );
 };
